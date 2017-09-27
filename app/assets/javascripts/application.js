81e91fe8a5b7d53f7ff7525ccbd56fff9261177a
Charlie Merrill <fcharliemerrill@gmail.com>
2017-09-27 10:58:39 -0400

    m

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,4 @@
 
 
 
-$(document).ready(function() {
-$("body").keyup(function(e){
-  if(e.keyCode === 77)
-    $("body").toggleClass("mole");
-});
-  $("body").mousemove(function(e){
-    $("body").css('background-position',(e.pageX - 250)+'px '+(e.pageY - 250)+'px');
-  });
-});
+
