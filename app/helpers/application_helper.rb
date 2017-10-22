df66f9f25f9b2df131c06a22b2e3f3a955c8f818
Charlie Merrill <fcharliemerrill@gmail.com>
2017-10-22 15:38:04 -0400

    fix heroku error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
   def user_avatar(user)
-    if user.profile_picture.present?
-      return user.profile_picture.path
-    elsif user.facebook_picture_url.present?
-      return user.facebook_picture_url
-    else
       return image_path 'avatar.jpg'
-    end
   end
 end
