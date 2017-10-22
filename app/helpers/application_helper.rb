097ecf413b57c20c22fa537203407ce104a1508b
Charlie Merrill <fcharliemerrill@gmail.com>
2017-10-22 15:47:11 -0400

    changed nav pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def user_avatar(user)
+    if user.profile_picture.present?
+      return user.profile_picture.path
+    elsif user.facebook_picture_url.present?
+      return user.facebook_picture_url
+    else
       return image_path 'avatar.jpg'
     end
   end
