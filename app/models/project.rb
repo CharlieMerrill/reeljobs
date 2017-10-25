1f2d8294a0c2de3406b1eccc156dc486d0337453
Charlie Merrill <fcharliemerrill@gmail.com>
2017-10-25 15:34:53 -0400

    added dependent destroy to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :host_reviews
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
   validates :schedule, presence: true
